c11ad4a5d21c9ddade7f48ca6be74244bca6738e
joshnyc11 <89551011+joshnyc11@users.noreply.github.com>
2022-05-13 20:15:55 -0400

    Add files via upload

---
+++ Programnum5.java
@@ -0,0 +1,28 @@
+package week5;
+
+
+// write a program that calculates all the powers of 2 from 1 to 10
+
+// powers of 2 would mean 1^2  using mat.pow is 
+
+// 1 to 2 power is 1
+//2 to 2 power is 
+public class Programnum5 {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		double pow = 1;
+		for (int i=1; i <= 10 ; i++) {
+			
+			
+		
+		 pow = Math.pow(i,2);
+				
+	}
+
+	System.out.println(pow);
+}
+	
+}
